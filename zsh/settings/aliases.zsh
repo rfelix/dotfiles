64a6347ff105875e7b5c2a12b15aa9e066a808bd
Raoul Felix <rf@rfelix.com>
2013-11-05 10:48:45 +0000

    Load more Prezto modules & add more ZSH aliases

--- zsh/settings/aliases.zsh
+++ zsh/settings/aliases.zsh
@@ -1,2 +1,11 @@
+# =========== Shell
+
+# Grep through whole history
+alias ghist="history 1 | grep"
+
+# Reload ZSH
+alias zr='source ~/.zshrc'
+
+# =========== Ruby-specific
 alias be="bundle exec"
 alias frb="foreman run bundle exec"

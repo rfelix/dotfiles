87722341ef3a689ec6044af0a36b8ec79466f77f
Raoul Felix <rf@rfelix.com>
2013-11-12 23:40:52 +0000

    Add alias to easily open Heroku logs for an app

--- zsh/settings/aliases.zsh
+++ zsh/settings/aliases.zsh
@@ -9,3 +9,7 @@ alias zr='source ~/.zshrc'
 # =========== Ruby-specific
 alias be="bundle exec"
 alias frb="foreman run bundle exec"
+
+# =========== Heroku-specific
+
+alias htail="heroku logs --tail --app" # usage: htail an-app
